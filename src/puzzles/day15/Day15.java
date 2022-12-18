3700552095d5b1ea36caeaaa489b63f9cc5f01e6
Sam Peterson <yoshiapolis@gmail.com>
2022-12-18 12:11:40 -0600

    Day 15!!

---
+++ src/puzzles/day15/Day15.java
@@ -0,0 +1,132 @@
+package puzzles.day15;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import main.Puzzle;
+import main.Solution;
+
+@Solution(day = 15)
+public class Day15 extends Puzzle {
+
+	private static final int NUM_SENSORS = 38;
+	private Sensor[] sensors;
+
+	
+	private void loadSensors(BufferedReader in) throws IOException {
+		int i = 0;
+		sensors = new Sensor[NUM_SENSORS];
+		
+		String line;
+		while((line = in.readLine()) != null) {
+			sensors[i ++] = new Sensor(line);
+		}
+	}
+	
+	private boolean beaconCanExist(int x, int y) {
+		boolean beaconCanExist = true;
+		for(Sensor sensor : sensors) {
+			if(!sensor.beaconCanExist(x, 2000000)) {
+				beaconCanExist = false;
+			}
+		}
+		
+		return beaconCanExist;
+	}
+	
+	private boolean distressBeaconCanExist(int x, int y) {
+		boolean distressBeaconCanExist = true;
+		for(Sensor sensor : sensors) {
+			if(!sensor.distressBeaconCanExist(x, 2000000)) {
+				distressBeaconCanExist = false;
+			}
+		}
+		
+		return distressBeaconCanExist;
+	}
+	
+	private ArrayList<Sensor> getNearSensors(int y) {
+		ArrayList<Sensor> nearSensors = new ArrayList<Sensor>();
+		for(Sensor sensor : sensors) {
+			int[] sensorPos = sensor.getSensorPos();
+			int dist = Math.abs(sensorPos[1]) - y;
+			
+			if(dist <= sensor.getCoveredRadius()) {
+				nearSensors.add(sensor);
+			}
+		}
+		
+		return nearSensors;
+	}
+	
+	private Sensor findFurthestInRangeSensor(int x, int y, ArrayList<Sensor> sensors) {
+		int largestDist = -1;
+		Sensor closestSensor = null;
+		
+		for(Sensor sensor : sensors) {
+			int dist = sensor.distanceTo(x, y);
+			if(dist <= sensor.getCoveredRadius() && dist > largestDist) {
+				largestDist = dist;
+				closestSensor = sensor;
+			}
+		}
+
+		return closestSensor;
+	}
+	
+	private int findDistressBeacon(int y) {
+		ArrayList<Sensor> nearSensors = getNearSensors(y);
+
+		int x = 0;
+		while(x <= 4000000) {
+			Sensor furthestInRange = findFurthestInRangeSensor(x, y, nearSensors);
+			if(furthestInRange == null) {
+				return x;
+			}
+			nearSensors.remove(furthestInRange);
+			
+			x = furthestInRange.getRightmostCoveredXPosition(y) + 1;
+		}
+		
+		return Integer.MAX_VALUE;
+	}
+	
+	private int getTuningFrequency(int x, int y) {
+		return 4000000 * x + y;
+	}
+	
+	@Override
+	protected void part1(BufferedReader in, BufferedWriter out) throws IOException {
+		loadSensors(in);
+		
+		int minX = Sensor.getLeftmostCoveredPosition();
+		int maxX = Sensor.getRightmostCoveredPosition();
+				
+		int numEliminatedPositions = 0;
+		
+		for(int x = minX; x <= maxX; x ++) {
+			if(!beaconCanExist(x, 2000000)) {
+				numEliminatedPositions ++;
+			}
+		}
+		
+		out.write(Integer.toString(numEliminatedPositions));
+	}
+
+	@Override
+	protected void part2(BufferedReader in, BufferedWriter out) throws IOException {
+		loadSensors(in);
+
+		for(int y = 0; y <= 4000000; y ++) {
+			int position = findDistressBeacon(y);
+			if(position != Integer.MAX_VALUE) {
+				out.write("x: " + position + ", y: " + y);
+				break;
+			}
+		}
+		
+	}
+
+}
---
+++ src/puzzles/day15/Sensor.java
@@ -0,0 +1,98 @@
+package puzzles.day15;
+
+public class Sensor {
+	
+	private static int leftmostSensor;
+	private static int rightmostSensor;
+	private static int highestCoverage;
+	
+	public static int getLeftmostCoveredPosition() {
+		return leftmostSensor - highestCoverage;
+	}
+	
+	public static int getRightmostCoveredPosition() {
+		return rightmostSensor + highestCoverage;
+	}
+	
+	public static int manhattanDist(int[] a, int[] b) {
+		return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+	}
+	
+	private int[] sensorPos;
+	private int[] beaconPos;
+	
+	private int coveredRadius;
+	
+	public Sensor(String line) {
+		String[] tokens = line.split(": ");
+		String sensorXY = tokens[0].substring(10);
+		String beaconXY = tokens[1].substring(21);
+		
+		this.sensorPos = parseCoordinates(sensorXY);
+		this.beaconPos = parseCoordinates(beaconXY);
+		
+		if(beaconPos[0] > rightmostSensor) {
+			rightmostSensor = beaconPos[0];
+		} else if(beaconPos[0] < leftmostSensor) {
+			leftmostSensor = beaconPos[0];
+		}
+		
+		this.coveredRadius = manhattanDist(sensorPos, beaconPos);
+		
+		if(coveredRadius > highestCoverage) {
+			highestCoverage = coveredRadius;
+		}
+	}
+	
+	public int[] getSensorPos() {
+		return this.sensorPos;
+	}
+	
+	public int[] getBeaconPos() {
+		return this.beaconPos;
+	}
+	
+	public int getCoveredRadius() {
+		return this.coveredRadius;
+	}
+	
+	public int getRightmostCoveredXPosition(int y) {
+		int dy = Math.abs(sensorPos[1] - y);
+		int localRadius = coveredRadius - dy;
+
+		return sensorPos[0] + localRadius;
+	}
+	
+	public boolean distressBeaconCanExist(int x, int y) {
+		if(beaconPos[0] == x && beaconPos[1] == y) return false;
+		
+		int dist = manhattanDist(new int[] {x, y}, sensorPos);
+		return (dist > coveredRadius);
+	}
+	
+	public boolean beaconCanExist(int x, int y) {
+		if(beaconPos[0] == x && beaconPos[1] == y) return true;
+		
+		int dist = manhattanDist(new int[] {x, y}, sensorPos);
+		return (dist > coveredRadius);	
+	}
+	
+	public boolean inRange(int x, int y) {
+		return distanceTo(x, y) <= coveredRadius;
+	}
+	
+	public int distanceTo(int x, int y) {
+		return manhattanDist(sensorPos, new int[] {x, y});
+	}
+	
+	private int[] parseCoordinates(String coordinates) {
+		String[] coordinateTokens = coordinates.split(", ");
+		String xString = coordinateTokens[0].substring(2);
+		String yString = coordinateTokens[1].substring(2);
+		
+		return new int[] {Integer.parseInt(xString), Integer.parseInt(yString)};
+	}
+	
+	
+	
+}
